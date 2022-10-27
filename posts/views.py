4bac71deefc830c037266546a16cc84d9f02a835
aizhamal27 <jamka.sad@gmail.com>
2022-10-27 14:01:07 +0600

    first commit

---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from posts.models import Post
+
+# Create your views here.
+
+def register(request):
+    return render(request, 'register1.html')
+def index(request):
+    posts = Post.objects.all ()
+    context = {
+        'posts' : posts
+    }
+    return render (request, 'index.html', context)
---
+++ users/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,redirect
+from users.models import User
+from django.contrib.auth import login, authenticate
+
+# Create your views here.
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        last_name = request.POST.get('last_name')
+        first_name = request.POST.get('first_name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        profile_image = request.FILES.get('profile_image')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        if password == confirm_password:
+            user = User.objects.create(username = username, last_name = last_name, first_name = first_name, email = email, phone = phone, profile_image = profile_image)
+            user.set_password(password)
+            user.save()
+            user = User.objects.get(username = username)
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('register')
+    return render(request, 'register1.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User.objects.get(username = username)
+        user = authenticate(username = username, password = password)
+        login(request, user)
+        return redirect('index')
+    return render(request, 'login1.html')
